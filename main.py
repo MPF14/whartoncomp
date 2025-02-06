b4050814001c44cfb6cd3f56a9c5136e90078ece
MPF14 <maskedpuppy27@gmail.com>
2025-02-06 01:19:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import pandas as pd
 import numpy as np
-df = pd.read_csv('/whartoncomp/data.csv')
-print(df.head(10))
-print('hi')
+
+df = pd.read_csv('/Users/mf/eh/whartoncomp/data.csv', skipinitialspace=True, na_values='?')
+
+df['win'] = (df['team_score'] > df['opponent_team_score']).astype(int)
+
+sdv = df.groupby(['home_away_NS', 'win']).agg(lambda x: x.std() if x.dtype in ['int64', 'float64'] else None)
+avg = df.groupby(['home_away_NS', 'win']).agg(lambda x: x.mean() if x.dtype in ['int64', 'float64'] else None)
+
+print(sdv)
+print(avg)
+
+# sdv = pd.DataFrame(sdv)
+# avg = pd.DataFrame(avg)
+# sdvpath = 'sdv.csv'
+# avgpath = 'avg.csv'
+# sdv.to_csv(sdvpath, index=False)
+# avg.to_csv(avgpath, index=False)
