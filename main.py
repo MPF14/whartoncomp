db4ea9fe7463d080b58bf541f8302404bd829e2b
MPF14 <maskedpuppy27@gmail.com>
2025-02-06 01:45:17 -0500

    Update main.py

--- main.py
+++ main.py
@@ -7,13 +7,20 @@ df['win'] = (df['team_score'] > df['opponent_team_score']).astype(int)
 
 sdv = df.groupby(['home_away_NS', 'win']).agg(lambda x: x.std() if x.dtype in ['int64', 'float64'] else None)
 avg = df.groupby(['home_away_NS', 'win']).agg(lambda x: x.mean() if x.dtype in ['int64', 'float64'] else None)
+tavg = df.groupby(['team','home_away_NS', 'win']).agg(lambda x: x.mean() if x.dtype in ['int64', 'float64'] else None)
 
 print(sdv)
 print(avg)
+print(tavg)
 
-# sdv = pd.DataFrame(sdv)
-# avg = pd.DataFrame(avg)
+# sdv = sdv.reset_index()
+# avg = avg.reset_index()
+# tavg = tavg.reset_index()
 # sdvpath = 'sdv.csv'
 # avgpath = 'avg.csv'
+# tavgpath = 'tavg.csv'
 # sdv.to_csv(sdvpath, index=False)
-# avg.to_csv(avgpath, index=False)
+# avg.to_csv(avgpath, index=False) 
+# tavg.to_csv(tavgpath, index=False)
+
+
