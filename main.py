f6c301c42b638bae261f69635a66dd8bbaa15909
MPF14 <maskedpuppy27@gmail.com>
2025-02-18 00:22:51 -0500

    almost done

--- main.py
+++ main.py
@@ -1,24 +1,28 @@
 import pandas as pd
 import numpy as np
 
-#test commit
-
-df = pd.read_csv('/Users/mf/eh/whartoncomp/data.csv', skipinitialspace=True, na_values='?')
-# commit test
-
+df = pd.read_csv('/Users/mf/eh/whartoncomp/data/data.csv', skipinitialspace=True, na_values='?')
 df['win'] = (df['team_score'] > df['opponent_team_score']).astype(int)
 
-sdv = df.groupby(['home_away_NS', 'win']).agg(lambda x: x.std() if x.dtype in ['int64', 'float64'] else None)
-avg = df.groupby(['home_away_NS', 'win']).agg(lambda x: x.mean() if x.dtype in ['int64', 'float64'] else None)
-tavg = df.groupby(['team','home_away_NS', 'win']).agg(lambda x: x.mean() if x.dtype in ['int64', 'float64'] else None)
+#team averages raw 
+tar = df.groupby(['team']).agg(lambda x: x.mean() if x.dtype in ['int64', 'float64'] else np.nan) 
+#standard deviation wins 
+sdw = df.groupby(['win']).agg(lambda x: x.std() if x.dtype in ['int64', 'float64'] else np.nan).drop([0])
+#win avgerages 
+wa = df.groupby(['win']).agg(lambda x: x.mean() if x.dtype in ['int64', 'float64'] else np.nan).drop([0])
+#standard deviation percent 
+sdp = (sdw/ wa * 2).replace([np.inf, -np.inf], np.nan)
+#team averages weighted 
+taw = tar.div(sdp.iloc[0]) 
+taw['talent'] = taw.sum(axis=1)
+taw = taw.sort_values(by='talent',ascending=False)
+print(taw)
 
-print(sdv)
-print(avg)
-print(tavg)
-
-# sdv = sdv.reset_index()
-# avg = avg.reset_index()
-# tavg = tavg.reset_index()
+#tawpath = '/Users/mf/eh/whartoncomp/data/taw.csv'
+#taw.to_csv(tawpath, index=True)
+#sdv = df.groupby(['home_away_NS', 'win']).agg(lambda x: x.std() if x.dtype in ['int64', 'float64'] else np.nan)
+#avg = df.groupby(['home_away_NS', 'win']).agg(lambda x: x.mean() if x.dtype in ['int64', 'float64'] else np.nan)
+#tavg = df.groupby(['team','home_away_NS', 'win']).agg(lambda x: x.mean() if x.dtype in ['int64', 'float64'] else np.nan)
 # sdvpath = 'data/sdv.csv'
 # avgpath = 'data/avg.csv'
 # tavgpath = 'data/tavg.csv'
